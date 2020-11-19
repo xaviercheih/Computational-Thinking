dbe2f899a4edb04246408239262eaa0052748bb8
xaviercheih <71419374+xaviercheih@users.noreply.github.com>
2020-11-19 11:22:21 +0800

    Add files via upload

---
+++ 陳漢杰＿week10-隨堂作業.py
@@ -0,0 +1,7 @@
+movie_title = "mont ball"
+release_year = "2020"
+
+print("電腦英文名稱",Kimetsu no Yaiba: Mugen Ressha-Hen)
+print("上映時間", release_year)
+print("電影片長", movie_time_min)
+print("電影評分", movie_rating)
